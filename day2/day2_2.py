a86d76d87e460a1bf31f7daa8ce3a10556216e98
Noe Marzat <noemarzat@gmail.com>
2023-12-05 15:13:06 +0100

    day2: finished

--- day2/day2_2.py
+++ day2/day2_2.py
@@ -4,7 +4,6 @@ import math
 data = open("input.txt", "r").read().split("\n")
 rgb = [12, 13, 14] # number of cube for each color
 
-
 # get the max number of cube for each color in a game
 # format : bag = {'r': 0, 'g': 0, 'b': 0}
 def getGameScore(line):
@@ -14,14 +13,8 @@ def getGameScore(line):
     return bag
 
 # Main loop
-id = 0
 result = 0
 for line in data:
-    id += 1
-    bag = getGameScore(line)
-
-    # if the number of cube for each color is less than the number of cube for each color
-    if bag['r'] <= rgb[0] and bag['g'] <= rgb[1] and bag['b'] <= rgb[2]:
-        result += id
+    result += math.prod(getGameScore(line).values())
 
 print(result)
