0d849d0aadecf463f8bd55774eddb727b2f5b89a
Velotome <noemarzat@gmail.com>
2023-12-10 10:42:57 +0100

    day3 : bug :(

--- day3/day3.py
+++ day3/day3.py
@@ -1,21 +1,29 @@
 import re
 
-data = open("input.txt", "r").read().split("\n")
+# bug somewhere in the code
+# Final value isn't correct, missing some values
 
-nonSymbols = "0123456789."
+data = open("input.txt", "r").read().split("\n")
 
 # Part 1 : Find all symbols
 symbols = {}
 for x in range(len(data[0])):
     for y in range(len(data)):
-        if data[y][x] not in nonSymbols:
-            symbols[(x,y)] = data[y][x]
+        if data[y][x] not in "0123456789.":
+            symbols[(x,y)] = []
 
-# Part 2 : Find all numbers
-numbers = {}
-line_number = 0
+
+# Part 2 : Find all numbers in intersectioon with symbols
+edge = []
+row = 0
 for line in data:
-    line_number += 1
+    row += 1
     for number in re.finditer(r'\d+', line):
-        # Store the number in a dict with key as (start_of_number-1, end_of_number+1, line_number)
-        numbers[(number.start()-1, number.end()+1, line_number)] = int(str(number.group(0)))
+        # compute all the coordinates that are considered "next to" the number
+        edge = {(row, c) for r in (row-1, row, row+1) for c in range(number.start()-1, number.end()+1)}
+        
+        for o in edge & symbols.keys():
+            symbols[o].append(int(number.group()))
+
+print(sum(sum(p) for p in symbols.values()))
+
